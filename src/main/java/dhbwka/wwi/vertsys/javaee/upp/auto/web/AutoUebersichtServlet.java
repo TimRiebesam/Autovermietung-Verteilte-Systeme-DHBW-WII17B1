3b29e40bac421210dd54440ae5d7e2418b5da1c9
Raphael Menken <raphthenuse@googleamil.com>
2019-04-05 11:48:40 +0200

    RM: Create AutoUebersichtServlet
    Das Servlet liest den Typ ein den der Kunde haben möchte und fragt bei Auto nach all den Autos nach und speichert sie in einer Liste die dann in der JSP durch eine foreach-schleife ausgelesen und gepostet wird.

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/upp/auto/web/AutoUebersichtServlet.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dhbwka.wwi.vertsys.javaee.upp.auto.web;
+
+/**
+ *
+ * @author rapht
+ */
+
+import dhbwka.wwi.vertsys.javaee.upp.auto.ejb.AutoBean;
+import dhbwka.wwi.vertsys.javaee.upp.auto.jpa.Auto;
+import java.io.IOException;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author TimRiebesam
+ */
+@WebServlet(urlPatterns = {"/app/uebersicht"})
+public class AutoUebersichtServlet extends HttpServlet{
+    
+    @EJB
+    private AutoBean autoBean;
+    
+    //@EJB
+    //private BuchungBean buchungBean;
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        Auto auto1;
+        //Liest den Typ der angefragten Autos aus der URL
+        String typ = request.getParameter("typ");
+        if(typ == null){
+            
+        }
+        else{
+            List<Auto> autos = autoBean.getAutoByTyp(typ);
+            autos.forEach(auto -> {
+              System.out.println(auto.getId());
+            });
+            
+            request.setAttribute("autos", autos);
+        }
+        
+        // Anfrage an die JSP weiterleiten
+        request.getRequestDispatcher("/WEB-INF/uebersicht/uebersicht_show.jsp").forward(request, response);
+    }
+    
+}
